f7e4c4b316f2b9ffb3cd40c647d351bf2c1cc078
slchahmed <108696292+slchahmed@users.noreply.github.com>
2023-03-14 23:23:53 +0000

    after terminated

--- src/app/dashboard/projet-info/projet-info.page.ts
+++ src/app/dashboard/projet-info/projet-info.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { collection, collectionData, Firestore, query, where } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { user } from '../auth.service';
 import { projet, ProjetService } from '../projet.service';
 
 @Component({
@@ -13,15 +17,51 @@ export class ProjetInfoPage implements OnInit {
   projet!:projet | null
   id!:string
   chek:boolean=false
-  constructor(private active_router:ActivatedRoute,private service:ProjetService,private alertcontroller:AlertController) { }
+  user!:user[] 
+  chef!:user
+  constructor(private auth:Auth,private active_router:ActivatedRoute,private service:ProjetService,private alertcontroller:AlertController,private firestore:Firestore) { }
 
   ngOnInit() {
     this.active_router.paramMap.subscribe(paramap=>{
      this.id= paramap.get('id') as string
     })
-    this.service.getprojetById(this.id).subscribe(res=>{
-      this.projet = res as projet;
+    this.service.getprojetById(this.id).subscribe(projet=>{
+   
+      if (projet['status'] == 'Not started') {
+        projet['badgeColor'] = 'hsl(58,100%,54%)';
+       
+      }
+       if ( projet['status'] == 'behind schedule') {
+        
+        projet['badgeColor'] = '#ff0404';
+     
+        
+
+      } 
+      if (projet['status'] =='In progress') {
+        projet['badgeColor'] = '#FDA349';
+    
+        
+
+      } 
+       if(projet['status'] == 'Completed'){
+        projet['badgeColor'] = '#55ad48';         
+
+      }
+      this.getuser().subscribe(user=>{
+        this.user = user
+      
+        this.chef = this.user[0]
+      
+        projet['chef'] = this.chef?.nom
+        this.projet = projet as projet;
+      })
+   
+      
+        
+     
     })
+  
     
   }
 
@@ -48,6 +88,13 @@ export class ProjetInfoPage implements OnInit {
   ],
   })
   await alert.present();
+}
+
+getuser(): Observable<user[]> {
+  const usermail = this.auth.currentUser?.email;
+  const usersRef = collection(this.firestore, 'users');
+  const q = query(usersRef, where("email", "==", usermail));
+  return collectionData(q, { idField: 'id' })as unknown as Observable<user[]>
 }
 
 }
